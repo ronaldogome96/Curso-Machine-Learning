bdcf6458baba07b90dc557215081cb3cf1bc7cc9
Ronaldo Gomes <gomes.ronaldo96@gmail.com>
2019-10-05 22:53:39 -0300

    Aula 17

--- pre_processamento_credit_data.py
+++ pre_processamento_credit_data.py
@@ -7,4 +7,18 @@ Created on Sat Oct  5 22:09:58 2019
 
 import pandas as pd
 base = pd.read_csv('credit_data.csv')
+#descreve os dados
 base.describe()
+#localiza algum valor na base de dados
+base.loc[base['age']<0]
+# apagar a coluna
+base.drop('age', 1 , inplace=True)
+# Apagar somenete os registros com problemas
+base.drop(base[base.age<0].index, inplace=True)
+#preeencher os valores manualmente
+# preencher esses valores com a media
+base.mean()
+base['age'].mean()
+base['age'][base.age>0].mean()
+base.loc[base.age<0 , 'age'] =40.92
+
