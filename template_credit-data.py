25778b78cc72bf2517cecf6392a03ac31f4c5f8f
Ronaldo Gomes <gomes.ronaldo96@gmail.com>
2019-10-06 01:21:12 -0300

    Aula 25

---
+++ template_credit-data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+base = pd.read_csv('credit_data.csv')
+base.loc[base.age < 0, 'age'] = 40.92
+               
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(previsores[:, 1:4])
+previsores[:, 1:4] = imputer.transform(previsores[:, 1:4])
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+#esse codigo nao rodou no meu spyder, afirmou um erro de
+#No module name sklearn.cross_validation
+from sklearn.cross_validation import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
