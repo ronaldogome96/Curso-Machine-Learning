23778a8905d447bc03da58bc633022ae4acb69e5
Ronaldo Gomes <gomes.ronaldo96@gmail.com>
2019-10-13 13:05:01 -0300

    Aula 124

--- validacao_cruzada_stratifiedkfold.py
+++ validacao_cruzada_stratifiedkfold.py
@@ -28,11 +28,12 @@ b = np.zeros(shape=(previsores.shape[0], 1))
 
 #Garante a melhor distribuição entre as classes
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 #Usa o K =10
 #random_state = 0 Faz os daods serem os mesmos, nao importa quantas vezes sejam gerados
 kfold = StratifiedKFold(n_splits = 10, shuffle = True, random_state = 0)
 resultados = []
+matrizes = []
 #Motrsa os indices com a validação cruzada
 for indice_treinamento, indice_teste, in kfold.split(previsores, b):
     #Printa os 10 cruzadas diferentes
@@ -42,8 +43,14 @@ for indice_treinamento, indice_teste, in kfold.split(previsores, b):
     classificador.fit(previsores[indice_treinamento], classe[indice_treinamento])
     previsoes = classificador.predict(previsores[indice_teste])
     precisao = accuracy_score(classe[indice_teste], previsoes)
+    #Cria a matriz de confusao para cada validação
+    matrizes.append(confusion_matrix(classe[indice_teste], previsoes))
     resultados.append(precisao)
+#Faz a matriz final com a media do resultado das anteriores
+matriz_final = np.mean(matrizes, axis = 0)
 #Tranforma a lista em tipo float
 resultados = np.asarray(resultados)
 resultados.mean()
 resultados.std()
+
+
