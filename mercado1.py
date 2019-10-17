fc0995651f0d12f46583eecbc314fe7a7be75f7a
Ronaldo Gomes <gomes.ronaldo96@gmail.com>
2019-10-16 23:48:36 -0300

    Aula 172

--- mercado1.py
+++ mercado1.py
@@ -12,14 +12,23 @@ for i in range(0, 10):
 
 #Importa a biblioteca apriori
 from apyori import apriori
+#Faz a geração de regras para a matriz transação
+#min_support = 0.3 é o suporte que define o calculo inicial
+#min_confidence = 0.8 é a confiança minima
+#min_lift = 2 é o numero minimo do lift
+#min_length = 2 é o numero minimo de atrubutos da sua regra
 regras = apriori(transacoes, min_support = 0.3, min_confidence = 0.8, min_lift = 2, min_length = 2)
 
+#Mostra as regras em lista
 resultados = list(regras)
 resultados
 
+#Cria uma segunda variavel resultado com uma melhor organizada, com uma separação por linha
 resultados2 = [list(x) for x in resultados]
 resultados2
+
+#Mostra as regras de forma ordenada, com o parametro range informando quantos voce quer
 resultadoFormatado = []
-for j in range(0, 5):
+for j in range(0, 3):
     resultadoFormatado.append([list(x) for x in resultados2[j][2]])
 resultadoFormatado
